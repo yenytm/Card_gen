f1c2f76f24d6d4b874a068e6ab85f384381cc453
yenytm <143008413+yenytm@users.noreply.github.com>
2023-10-23 19:54:58 -0400

    fix btn

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 window.onload = () => {
-    
-    const cardSuit = ["♦️", "♥️", "♠️", "♣️"];
-    const cardNumber = [
+  const cardSuit = ["♦", "♥", "♠", "♣"];
+  const cardNumber = [
     "A",
     "2",
     "3",
@@ -14,37 +13,28 @@ window.onload = () => {
     "10",
     "J",
     "Q",
-    "K"
-    ];
+    "K",
+  ];
 
-
+  document.getElementById("clickButton").addEventListener("click", () => {
     let generateRandomNumber = () => {
-        let number = Math.floor(Math.random()*cardNumber.length)
-        return cardNumber[number]
-    }
+      let number = Math.floor(Math.random() * cardNumber.length);
+      return cardNumber[number];
+    };
     let generateRandomSuit = () => {
-        let suit = Math.floor(Math.random()*cardSuit.length)
-        return cardSuit[suit]
-
-        
-    }
-
-    document.getElementById("numero").innerHTML = generateRandomNumber()
-
-    const suit = generateRandomSuit ()
-
-    document.getElementById("topSuit").innerHTML = suit
-    document.getElementById("bottomSuit").innerHTML = suit
+      let suit = Math.floor(Math.random() * cardSuit.length);
+      return cardSuit[suit]
+    };
 
 
+    document.getElementById("numero").innerHTML = generateRandomNumber();
+
+    const suit = generateRandomSuit();
+
+    document.getElementById("topSuit").innerHTML = suit;
+
+    document.getElementById("bottomSuit").innerHTML = suit;
 
 
-
-    document.getElementById("clickButton").addEventListener("click")
-
-
-}
-
-
-
-    
+  });
+};
