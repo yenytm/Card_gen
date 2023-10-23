3e5b6c1f00397143c7dd16586812b6f5f2d5c933
yenytm <143008413+yenytm@users.noreply.github.com>
2023-10-23 02:35:56 -0400

    bottom suit need help

--- app.js
+++ app.js
@@ -1,23 +1,47 @@
-
 window.onload = () => {
-    // let cardSuit = document.getElementsByClassName("icon") 
-    let cardVal = document.getElementsByClassName("card_number")
-
-let generateRandomSuit = () => {
     
+    const cardSuit = ["♦️", "♥️", "♠️", "♣️"];
+    const cardNumber = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+    ];
 
-    let suit = ["♦", "♥", "♠", "♣"]
-    let indexSuit = math.floor(math.random()*suit.length)
-    return suit[indexSuit]
 
-}    
-let generateRandomNumber = () => {
-    let cardValue = ["2", "3", "4","5", "6", "7","8", "9", "10","K", "Q", "J", "A"]
-    let indexCard = math.floor(math.random()*cardValue.length)
-    return cardValue[indexCard]
+    let generateRandomNumber = () => {
+        let number = Math.floor(Math.random()*cardNumber.length)
+        return cardNumber[number]
+    }
+    let generateRandomSuit = () => {
+        let suit = Math.floor(Math.random()*cardSuit.length)
+        return cardSuit[suit]
+
+        
+    }
+
+    document.getElementById("numero").innerHTML = generateRandomNumber()
+    document.getElementById("suitSymbol").innerHTML = generateRandomSuit()
+    // document.getElementById("suitSymbol2").innerHTML = generateRandomSuit()
+
+
+
+
+
+    document.getElementById("clickButton").addEventListener("click")
+
+
 }
+
+
+
     
-}
-
-
-
